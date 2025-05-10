c18781fa544a2bdc98060ebfff0c8420e51ee0a7
nidhisingh5958 <nidhisingh5958@gmail.com>
2025-05-10 17:27:24 +0530

    fixing routing , scan qr screen and my appointments

--- lib/utils/routes/router.dart
+++ lib/utils/routes/router.dart
@@ -11,7 +11,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:CuraDocs/utils/routes/route_constants.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:CuraDocs/features/auth/landing/role.dart';
 import 'package:CuraDocs/features/auth/screens/login/forgot_pass.dart';
 import 'package:CuraDocs/features/auth/screens/login/login_screen.dart';
